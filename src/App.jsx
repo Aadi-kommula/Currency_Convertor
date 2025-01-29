d31985697e55a63b0f4755fd99619f6b4a093d60
Aadi-kommula <aadikommula@gmail.com>
2025-01-30 00:42:51 +0530

    modified ui and heading section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { useEffect } from 'react'
 import { useState } from 'react'
+import './App.css'
 function App() {
   const [currencies, setcurrencies] = useState('')
   const [FromCurrency, setFromCurrency] = useState('1inch')
   const [ToCurrency, setToCurrency] = useState('1inch')
-  const [FromValue,setFromValue]=useState(0)
-  const [ToValue,setToValue]=useState()
+  const [FromValue, setFromValue] = useState(1)
+  const [ToValue, setToValue] = useState(1)
 
   //fecthing currency countries data
 
@@ -21,41 +22,47 @@ function App() {
   //Caluclating currency values 
 
   function Caluclate() {
-      fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${FromCurrency}.json`)
-      .then((response)=>response.json())
-      .then((data)=>{
-        setToValue(FromValue*data[FromCurrency][ToCurrency])
-        
+    fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${FromCurrency}.json`)
+      .then((response) => response.json())
+      .then((data) => {
+        setToValue(FromValue * data[FromCurrency][ToCurrency])
+
       })
   }
   return (
     <>
-      {/* from section */}
-      <div className='flex justify-center items-center flex-col '>
-        <h1>My First React App</h1><br />
-        <div id="select" className='w-92 h-auto bg-gray-300 flex flex-col'>
-          <label htmlFor="select" className='m-3'>From</label>
-          <select className='w-30 m-2 bg-white' name="" id="from" onChange={(e) => setFromCurrency(e.target.value)}>
-            {Object.entries(currencies).map(([currencyCode, CurrencycCountry]) =>
-              <option key={currencyCode} value={currencyCode}>
-                {CurrencycCountry}
-              </option>)}
-          </select>
-          <input className='bg-white m-2' type="tel" onChange={(e)=>{setFromValue(e.target.value)}} placeholder='Enter a value ' name="input from" id="" />
-          <button className='bg-green-500 w-22 p-0 m-2' onClick={Caluclate} >Calculate</button>
+      <div className='flex justify-center items-center flex-col h-screen '>
+        <div id="select" className='w-auto h-auto bg-white flex flex-col rounded pl-16 pr-16 pt-5 pb-7'>
+          <heading className='m-2 '>Currency Convertor</heading>
+
+          {/* Display text  */}
+          <h3>{FromValue} {currencies[FromCurrency]} equals </h3>
+          <h1>{ToValue} {currencies[ToCurrency]}</h1>
+
+          <div className="flex-row">
+            <input type="tel"
+              onChange={(e) => { setFromValue(e.target.value) }}
+              placeholder='Enter the amount of currency '
+              name="input from" id="" /><br />
+              
+            <select className='w-30 h-10 bg-white' name="" id="from" onChange={(e) => setFromCurrency(e.target.value)}>
+              {Object.entries(currencies).map(([currencyCode, CurrencycCountry]) =>
+                <option key={currencyCode} value={currencyCode}>
+                  {CurrencycCountry}
+                </option>)}
+            </select>
+            <arrow>→</arrow>
+            <select className='w-30 h-10 mx-3 bg-white' name="" id="from" onChange={(e) => { setToCurrency(e.target.value) }}>
+              {Object.entries(currencies).map(([currencyCode, CurrencycCountry]) =>
+                <option key={currencyCode} value={currencyCode}>
+                  {CurrencycCountry}
+                </option>)}
+            </select>
+            {/* <input className='bg-white m-2' type="tel" value={ToValue} disabled name="input from" id="" /> */}
+          </div>
+          <button className='bg-gray-200 ' onClick={Caluclate} >Fetch Data</button>
         </div>
-        <br />
-        {/* To section  */}
-        <div id="select" className='w-92 h-auto bg-gray-300 flex flex-col'>
-          <label htmlFor="select" className='m-3'>To</label>
-          <select className='w-30 m-2 bg-white' name="" id="from" onChange={(e) => { setToCurrency(e.target.value) }}>
-            {Object.entries(currencies).map(([currencyCode, CurrencycCountry]) =>
-              <option key={currencyCode} value={currencyCode}>
-                {CurrencycCountry}
-              </option>)}
-          </select>
-          <input className='bg-white m-2' type="tel" value={ToValue} disabled name="input from" id="" />
-        </div>
+
       </div>
     </>
   )
